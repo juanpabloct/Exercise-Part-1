dfa4a312b98cffc80c2df3967b47f4843a1c5b11
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-04-21 17:13:27 -0500

    Se hizo el ejercicio requerido de los numeros, los numeros multiplos de 5 van a decir bazz y los que no buzz

---
+++ buzzBazz.js
@@ -0,0 +1,9 @@
+let iterador = 0;
+while (iterador <= 100) {
+  if (iterador % 5 == 0) {
+    console.log(iterador + " Bazz");
+  } else {
+    console.log(iterador + " Buzz");
+  }
+  iterador++;
+}
